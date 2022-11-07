2301d86e726494a8945d8bc39927cae1307a313c
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2022-11-06 22:30:08 -0800

    add a lot of stuff to my config file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,51 @@
-(add-to-list 'load-path' "~/.emacs.d/evil")
+ ;; (add-to-list 'load-path' "~/.emacs.d/evil")
+
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
 (require 'evil)
-(evil-mode 1)
+(evil-mode t)
+(evil-set-initial-state 'Custom-mode 'normal)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package markdown-mode
+  :ensure t)
+(use-package magit
+  :ensure t)
+(use-package evil
+  :ensure t)
+(use-package solarized-theme
+  :ensure t)
+(use-package helm
+  :ensure t)
+(use-package evil-commentary
+  :ensure t)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 158 :width normal)))))
+
+(load-theme 'solarized-dark t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-commentary helm markdown-mode use-package solarized-theme magit evil)))
