9a264a17e734b89eac5ea52d758aa85650bf1304
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2022-11-09 17:56:54 -0800

    add location for notes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,6 +58,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Documents/college-notes/MASTER_AGENDA.org"))
  '(package-selected-packages
    '(undo-fu undo-tree evil-collection evil-commentary helm markdown-mode use-package solarized-theme magit evil)))
 
